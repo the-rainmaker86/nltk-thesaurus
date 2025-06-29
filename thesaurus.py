720946e79cb47ad688233882f7cb0cd8ba41a1f0
the-rainmaker86 <158232636+the-rainmaker86@users.noreply.github.com>
2025-06-29 14:54:36 -0400

    Update thesaurus.py

--- thesaurus.py
+++ thesaurus.py
@@ -8,19 +8,9 @@ def longest_word(word_):
     lemmas.sort(key=lambda x: len(x))
     return lemmas[-1] if lemmas else word_
 
-# example = "I am writing my blog post about something I wrote and I hope you enjoy it"
-# example = "op is a faggot"
-# example = '''i hadnt thought of it that way before but i bet it would be pretty easy to decode'''
-# x = 'risk of rain returns'
 
-example  = re.sub(r'[,.?\-\']', '', '''What the fuck did you just fucking say about me, you little bitch? I'll have you know I graduated top of my class in the Navy Seals, and I've been involved in numerous secret raids on Al-Qaeda, and I have over three hundred confirmed kills. I am trained in gorilla warfare and I'm the top sniper in the entire US armed forces. You are nothing to me but just another target. I will wipe you the fuck out with precision the likes of which has never been seen before on this Earth, mark my fucking words. You think you can get away with saying that shit to me over the Internet? Think again, fucker. As we speak I am contacting my secret network of spies across the USA and your IP is being traced right now so you better prepare for the storm, maggot. The storm that wipes out the pathetic little thing you call your life. You're fucking dead, kid. I can be anywhere, anytime, and I can kill you in over seven hundred ways, and that's just with my bare hands. Not only am I extensively trained in unarmed combat, but I have access to the entire arsenal of the United States Marine Corps and I will use it to its full extent to wipe your miserable ass off the face of the continent, you little shit. If only you could have known what unholy retribution your little "clever" comment was about to bring down upon you, maybe you would have held your fucking tongue. But you couldn't, you didn't, and now you're paying the price, you goddamn idiot. I will shit fury all over you and you will drown in it. You're fucking dead, kiddo.''').lower()
 
-x = '''
-Fuck fuck fuck a duck
-Screw a kangaroo
-Finger bang an orangutan
-At your local zoo
-'''
+test = "your sentence goes here"
 
-print(' '.join(longest_word(word) for word in x.split()))
+print(' '.join(longest_word(word) for word in test.split()))
 
